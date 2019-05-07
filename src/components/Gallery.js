f667eb08241b069050d7371cccdd48015d153ca0
Nick-Hericks <nickhericks@gmail.com>
2019-05-07 06:27:19 +0200

    Add components

---
+++ src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Item from "./Item";
+
+const Gallery = () => {
+  return (
+    <div>
+      <div>Gallery</div>
+
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Search from './Search';
+import Nav from './Nav';
+
+const Header = () => {
+
+	return (
+		<div>
+			<h2>React Gallery App</h2>
+			<Search />
+			<Nav />
+		</div>
+	)
+};
+
+
+
+export default Header;
